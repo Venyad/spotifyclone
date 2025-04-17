1af3e5a0d2854d843c92b314aab29a06eb121590
Venya <venyadurgam25@gmail.com>
2025-04-16 22:56:00 -0500

    Updated dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import { Button } from "./components/ui/button.js";
+
 function App(){
   return (
     <>
+    <h1 className="text-red-500 text-5xl">Hello</h1>
+    <Button variant={"outline"}>This is a button</Button>
     </>
   )
 }
