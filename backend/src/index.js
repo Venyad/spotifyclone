ef55c4bad9fe02dc0be94c9bc59a7d8c6592ad05
Venya <venyadurgam25@gmail.com>
2025-04-26 23:13:19 -0500

    Updated index.js file

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(5000,() => {
+    console.log("Server is running on port 5000")
+})
